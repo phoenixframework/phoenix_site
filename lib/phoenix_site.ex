dc3b5d8c1dcc62d4b5fb39176be2486467bf3d52
Gary Rennie <gazler@gmail.com>
2017-07-24 18:30:28 +0100

    chore(setup): initial commit

---
+++ lib/phoenix_site.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixSite do
+  @moduledoc """
+  Documentation for PhoenixSite.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixSite.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_site_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixSiteTest do
+  use ExUnit.Case
+  doctest PhoenixSite
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
